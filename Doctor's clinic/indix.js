24a9343fb9b8b79e3ebefc0b8809d345486b34a8
hosam3574 <hossamtradat@gmail.com>
2025-04-30 20:35:34 +0300

    pppp

---
+++ Doctor's clinic/indix.js
@@ -0,0 +1,8 @@
+function toggleInfo() {
+    var infoDiv = document.getElementById("info");
+    if (infoDiv.style.display === "none") {
+        infoDiv.style.display = "block";
+    } else {
+        infoDiv.style.display = "none";
+    }
+}
